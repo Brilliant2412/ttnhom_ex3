874d733df63acbc671f63b24af162a91b038bae4
Hung <miaooo.sl.ys.lyn.hyeon.lb@gmail.com>
2021-06-29 04:44:57 +0700

    oke

--- CODE/QuanLyThuVien/ThemMuonTra.cs
+++ CODE/QuanLyThuVien/ThemMuonTra.cs
@@ -25,7 +25,7 @@ namespace QuanLyThuVien
 
         private void ThemMuonTra_Load(object sender, EventArgs e)
         {
-            
+            //"SELECT SoThe from TheThuVien where NgayHetHan >= GETDATE()"
             DataTable theTable1 = new Database().SelectData("SELECT SoThe from TheThuVien");
             cbbSoThe.DisplayMember = "SoThe";
             cbbSoThe.ValueMember = "SoThe";
@@ -66,81 +66,90 @@ namespace QuanLyThuVien
             DateTime dtTra = dtPickerTra.Value;
             string soThe = cbbSoThe.Text;
             string tenSach = cbcTenSach.Text;
-            if (DateTime.Compare(dtMuon,dtTra) <= 0)
+
+            var r = new Database().Select(string.Format("SelectNgayHetHanTTV '" + soThe + "'"));
+            DateTime theTTV = Convert.ToDateTime(r["NgayHetHan"].ToString());
+            if (DateTime.Compare(theTTV,DateTime.Now) <=0)
             {
-                List<CustomParameter> lstPara = new List<CustomParameter>();
-
-                if (string.IsNullOrEmpty(maMuon))
-                {
-                    sql = "InsertMTandCTMT";
-                    lstPara.Add(new CustomParameter()
-                    {
-                        key = "@MaMM",
-                        value = addMM
-                    });
-                }
-                else
-                {
-                    sql = "UpdateMTandCTMT";
-                    lstPara.Add(new CustomParameter()
-                    {
-                        key = "@MaMM",
-                        value = maMuon
-                    });
-                    lstPara.Add(new CustomParameter()
-                    {
-                        key = "@TenSachCu",
-                        value = tenSachCu
-                    });
-                }
-
-                lstPara.Add(new CustomParameter()
-                {
-                    key = "@ngayMuon",
-                    value = dtMuon.ToString("yyyy-MM-dd")
-                });
-
-                lstPara.Add(new CustomParameter()
-                {
-                    key = "@ngayHenTra",
-                    value = dtTra.ToString("yyyy-MM-dd")
-                });
-
-                lstPara.Add(new CustomParameter()
-                {
-                    key = "@soThe",
-                    value = soThe
-                });
-                lstPara.Add(new CustomParameter()
-                {
-                    key = "@TenSach",
-                    value = tenSach
-                });
-
-                var rs = new Database().execute(sql, lstPara);
-                if (rs >= 1)
-                {
-                    if (string.IsNullOrEmpty(maMuon))
-                    {
-                        MessageBox.Show("Thêm mới thành công");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Cập nhật thành công");
-                    }
-                    this.Dispose();
-                }
-                else
-                {
-                    MessageBox.Show("Thao tác không thành công");
-                }
+                MessageBox.Show("Bạn nên gia hạn thẻ thư viện thẻ thư viện hết hạn hoặc nốt hôm nay hết hạn");
             }
             else
             {
-                MessageBox.Show("Vui lòng chọn lại thời gian hẹn trả, thời gian hẹn trả không được nhỏ hơn thời gian mượn");
+                if (DateTime.Compare(dtMuon, dtTra) <= 0)
+                {
+                    List<CustomParameter> lstPara = new List<CustomParameter>();
+
+                    if (string.IsNullOrEmpty(maMuon))
+                    {
+                        sql = "InsertMTandCTMT";
+                        lstPara.Add(new CustomParameter()
+                        {
+                            key = "@MaMM",
+                            value = addMM
+                        });
+                    }
+                    else
+                    {
+                        sql = "UpdateMTandCTMT";
+                        lstPara.Add(new CustomParameter()
+                        {
+                            key = "@MaMM",
+                            value = maMuon
+                        });
+                        lstPara.Add(new CustomParameter()
+                        {
+                            key = "@TenSachCu",
+                            value = tenSachCu
+                        });
+                    }
+
+                    lstPara.Add(new CustomParameter()
+                    {
+                        key = "@ngayMuon",
+                        value = dtMuon.ToString("yyyy-MM-dd")
+                    });
+
+                    lstPara.Add(new CustomParameter()
+                    {
+                        key = "@ngayHenTra",
+                        value = dtTra.ToString("yyyy-MM-dd")
+                    });
+
+                    lstPara.Add(new CustomParameter()
+                    {
+                        key = "@soThe",
+                        value = soThe
+                    });
+                    lstPara.Add(new CustomParameter()
+                    {
+                        key = "@TenSach",
+                        value = tenSach
+                    });
+
+                    var rs = new Database().execute(sql, lstPara);
+                    if (rs >= 1)
+                    {
+                        if (string.IsNullOrEmpty(maMuon))
+                        {
+                            MessageBox.Show("Thêm mới thành công");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Cập nhật thành công");
+                        }
+                        this.Dispose();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Thao tác không thành công");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng chọn lại thời gian hẹn trả, thời gian hẹn trả không được nhỏ hơn thời gian mượn");
+                }
+
             }
-            
-            
         }
 
         private void btnHuy_Click(object sender, EventArgs e)
